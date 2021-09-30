8a6449419abea31dda8ebd55ea6fdc34f1035254
AndrewEhrhardt <andyehrhardt@gmail.com>
2021-09-30 16:23:29 -0500

    fresh commit

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
+import SearchItem from '../SearchItem/SearchItem'
 
 function Search(){
     const [newSearch, setNewSearch] = useState('')
@@ -11,6 +11,7 @@ function Search(){
         event.preventDefault()
         axios.post('/api/giphy', {search: newSearch})
         .then(response => {
+            console.log(response.data)
             console.log(response.data[0].images.original.url)
             setResults(response.data)
         }).catch(error => {
@@ -24,6 +25,11 @@ function Search(){
         <input placeholder="Search" type='text' value={newSearch} onChange={(event) => setNewSearch(event.target.value)} />
         <input type='submit' value='Submit New Search' />
         </form>
+        <div>
+        {results.map((gif) => (
+            <SearchItem key={gif.images.original.url} image={gif.images.original.url}/>
+         ))}
+        </div>
         </>
     )
 }
---
+++ src/components/SearchItem/SearchItem.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+
+// image is name of props
+function SearchItem(props) {
+
+    return(
+        <>
+
+        </>
+    )
+}
+
+export default SearchItem;
