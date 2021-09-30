a552c2d25c48e7433be453065e02ec404e359d35
AndrewEhrhardt <andyehrhardt@gmail.com>
2021-09-30 10:19:48 -0500

    installs done and sagas working

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App(props) {
+  const dispatch = useDispatch();
+  const reduxState = useSelector(reduxState => reduxState)
+  
+  //On page load get favorites
+  useEffect(() => {
+    getFavorites();
+  }, []);
+
+  //Dispatch to saga for axios request
+  const getFavorites = () => {
+    dispatch({ type: 'GET_FAVORITES' })
+  }
+
+
+
   return (
     <div>
       <h1>Giphy Search!</h1>
+      {/* routes go here */}
+      {/*  GET axios request for search*/}
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects' 
+import logger from 'redux-logger';
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+//GET saga function
+function* getFavorites(){
+    //try{
+    yield console.log('Sagas are workin')
+    //}catch(err){
+
+    //}
+}
+
+
+//POST saga function
+
+
+
+// favoriteGiphy reducer
+const favoriteGiphyReducer = (state = [], action) => {
+    return state;
+}
+
+//Watcher Saga
+function* watcherSaga() {
+    //GET_FAVORITES takeEvery
+    yield takeEvery('GET_FAVORITES', getFavorites);
+    //ADD_FAVORITES takeEvery
+
+    
+
+
+}
+
+
+
+
+//
+const sagaMiddleware = createSagaMiddleware();
+//
+//create store
+const storeInstance = createStore(
+    combineReducers({
+        //reducers go here
+        favoriteGiphyReducer,
+    }),
+    applyMiddleware(sagaMiddleware, logger),
+);
+
+
+
+sagaMiddleware.run(watcherSaga);
+
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
